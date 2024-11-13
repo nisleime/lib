71b37f05212db648f81620687c0fccbd89e1b078
nisleime <119459202+nisleime@users.noreply.github.com>
2024-11-13 18:36:27 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,6 +1,21 @@
 <?php
 try	{
-	$dbh = new PDO ( "mysql:host=172.20.0.4;port=3306;dbname=ativa;charset=utf8", "root", "Ncm@647534",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));	
+try {
+    $dsn = 'mysql:host=104.234.173.105;dbname=Liberacao';
+    $username = 'root';
+    $password = 'Ncm@647534';
+
+    // Estabelece a conexão
+    $pdo = new PDO($dsn, $username, $password);
+    // Configura o PDO para lançar exceções em caso de erro
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo 'Conexão bem-sucedida!';
+} catch (PDOException $e) {
+    echo 'Erro na conexão: ' . $e->getMessage();
+}
+	
+/*	$dbh = new PDO ( "mysql:host=172.20.0.4;port=3306;dbname=ativa;charset=utf8", "root", "Ncm@647534",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));	
 
 /*$dbh = new PDO ( "mysql:host=remotemysql.com;dbname=a8jRiePG4R;charset=utf8", "a8jRiePG4R", "pczYGq1lnT",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));*/
 
@@ -10,11 +25,11 @@ try	{
  /*	$dbh = new PDO ( "mysql:host=mysql.freehostia.com;port=3306;dbname=nismar1_ativador;charset=utf8", "nismar1_ativador", "Ncm@647534",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")); 
 /*$dbh = new PDO ( "mysql:host=localhost;port=3306;dbname=nismar1_ativador;charset=utf8", "nismar1_ativador", "Ncm@647534",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));*/
 
-}
+//}
 
-catch(PDOexeception $e){ 
-echo $e->getMessage();
-	}
+//catch(PDOexeception $e){ 
+//echo $e->getMessage();
+//	}
 ?>
 
 
